42955a29dd38420930f385e3110ec511a983e693
Gabriel Albuquerque <fcogabrielmachadoalbuquerque@gmail.com>
2025-02-10 15:51:17 -0300

    Sending files

---
+++ demo-security/src/main/java/com/gmg/systemweb/security/web/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.gmg.systemweb.security.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	// abrir pagina home
+	@GetMapping({"/","/home"})
+	public String home() {
+		return "home";
+	}	
+	
+	// abrir página login
+	@GetMapping({"/login"})
+	public String login() {
+		return "login";
+	}
+		
+	// login inválido
+	@GetMapping({"/login-error"})
+	public String loginError(ModelMap model) {
+		model.addAttribute("alerta", "erro");
+		model.addAttribute("titulo", "Crendenciais inválidas!");
+		model.addAttribute("texto", "Login ou senha incorretos, tente novamente");
+		model.addAttribute("subtexto", "Acesso permitido apenas para cadastros já ativados.");
+		return "login";
+	}
+}
