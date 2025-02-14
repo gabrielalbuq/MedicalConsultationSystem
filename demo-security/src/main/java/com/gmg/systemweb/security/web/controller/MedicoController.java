dc18d8baf8a23f178d1e547078de98d167ebf8cf
Gabriel Albuquerque <fcogabrielmachadoalbuquerque@gmail.com>
2025-02-14 00:28:41 -0300

    Inicializando o perfil do médico e cadastrando seus dados

--- demo-security/src/main/java/com/gmg/systemweb/security/web/controller/MedicoController.java
+++ demo-security/src/main/java/com/gmg/systemweb/security/web/controller/MedicoController.java
@@ -1,6 +1,8 @@
 package com.gmg.systemweb.security.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gmg.systemweb.security.domain.Medico;
+import com.gmg.systemweb.security.domain.Usuario;
 import com.gmg.systemweb.security.service.MedicoService;
+import com.gmg.systemweb.security.service.UsuarioService;
 
 @Controller
 @RequestMapping("medicos")
@@ -17,24 +21,33 @@ public class MedicoController {
 
 	@Autowired
 	private MedicoService service;
+	@Autowired
+	private UsuarioService usuarioService;
 	
-	//abrir pagina de dados pessoais de medicos pelo MEDICO
+	// abrir pagina de dados pessoais de medicos pelo MEDICO
 	@GetMapping({"/dados"})
-	public String abrirPorMedico(Medico medico, ModelMap model) {
-	
+	public String abrirPorMedico(Medico medico, ModelMap model, @AuthenticationPrincipal User user) {
+		if (medico.hasNotId()) {
+			medico = service.buscarPorEmail(user.getUsername());
+			model.addAttribute("medico", medico);
+		}
 		return "medico/cadastro";
-	}	
+	}
 	
-	//salvar medico
+	// salvar medico
 	@PostMapping({"/salvar"})
-	public String salvar(Medico medico, RedirectAttributes attr) {
+	public String salvar(Medico medico, RedirectAttributes attr, @AuthenticationPrincipal User user) {
+		if (medico.hasNotId() && medico.getUsuario().hasNotId()) {
+			Usuario usuario = usuarioService.buscarPorEmail(user.getUsername());
+			medico.setUsuario(usuario);
+		}
 		service.salvar(medico);
 		attr.addFlashAttribute("sucesso", "Operação realizada com sucesso.");
 		attr.addFlashAttribute("medico", medico);
 		return "redirect:/medicos/dados";
-	}	
+	}
 		
-	//editar medico
+	// editar medico
 	@PostMapping({"/editar"})
 	public String editar(Medico medico, RedirectAttributes attr) {
 		service.editar(medico);
