7141dfda963474af94d030a45fe459cc645b99f7
Gabriel Albuquerque <fcogabrielmachadoalbuquerque@gmail.com>
2025-02-15 12:24:40 -0300

    implementing the registration confirmation email

--- demo-security/src/main/java/com/gmg/systemweb/security/DemoSecurityApplication.java
+++ demo-security/src/main/java/com/gmg/systemweb/security/DemoSecurityApplication.java
@@ -1,17 +1,29 @@
 package com.gmg.systemweb.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.gmg.systemweb.security.service.EmailService;
+
 @SpringBootApplication
-public class DemoSecurityApplication {
+public class DemoSecurityApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 
 		System.out.println(new BCryptPasswordEncoder().encode("123456"));
 
 		SpringApplication.run(DemoSecurityApplication.class, args);
+	}
+
+	@Autowired
+	EmailService service;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		service.enviarPedidoDeConfirmacaoDeCadastro("contato.gabriell021@gmail.com", "9871df");
 	}
 }
